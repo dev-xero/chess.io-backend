95d843d30785ed7a416f32cf6a65dadecd96ea56
dev-xero <elo48858@gmail.com>
2024-10-14 14:24:28 +0100

    devops: Configure terraform to use aws app runner

--- main.tf
+++ main.tf
@@ -1,97 +1,58 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "4.52.0"
-    }
-  }
-}
-
 provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_ecr_repository" "chessio_backend" {
-  name = "chessio_backend"
-}
+# Setup IAM Roles
+resource "aws_iam_role" "apprunner_role" {
+  name = "apprunner-ecr-role"
 
-resource "aws_security_group" "web_sg" {
-  name        = "web_sg"
-  description = "Allow HTTP inbound traffic"
-  vpc_id      = aws_vpc.main.id
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-resource "aws_ecs_cluster" "chessio_cluster" {
-  name = "chessio-cluster"
-}
-
-resource "aws_ecs_task_definition" "chessio" {
-  family                   = "chessio-backend"
-  network_mode             = "awsvpc"
-  requires_compatibilities = ["FARGATE"]
-  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
-  cpu                      = "256"
-  memory                   = "512"
-  container_definitions    = <<DEFINITION
-[
-  {
-    "name": "chessio-backend",
-    "image": "${aws_account_id}.dkr.ecr.${aws_region}.amazonaws.com/${aws_ecr_repository.chessio_backend.name}",
-    "cpu": 256,
-    "memory": 512,
-    "essential": true,
-    "portMappings": [
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
       {
-        "containerPort": 8080,
-        "hostPort": 8080
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "build.apprunner.amazonaws.com"
+        }
       }
     ]
-  }
-]
-DEFINITION
+  })
 }
 
-resource "aws_ecs_service" "chessio_backend_service" {
-  name            = "chessio-backend-service"
-  cluster         = aws_ecs_cluster.node_app_cluster.id
-  task_definition = aws_ecs_task_definition.node_app.arn
-  desired_count   = 1
-  launch_type     = "FARGATE"
+resource "aws_iam_policy_attachment" "apprunner_policy_attachment" {
+  name       = "apprunner-ecr-policy-attachment"
+  roles      = [aws_iam_role.apprunner_role.name]
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSAppRunnerServicePolicyForECRAccess"
+}
 
-  network_configuration {
-    subnets         = aws_subnet.private.*.id
-    security_groups = [aws_security_group.web_sg.id]
+# Create an App Runner service
+resource "aws_apprunner_service" "chessio_backend" {
+  service_name = "chessio-backend"
+
+  source_configuration {
+    image_repository {
+      image_configuration {
+        port = "8080"
+      }
+      image_identifier      = "039612890043.dkr.ecr.us-east-1.amazonaws.com/awsxero/chessio-backend:latest"
+      image_repository_type = "ECR"
+    }
+    
+    authentication_configuration {
+      access_role_arn = aws_iam_role.apprunner_role.arn
+    }
+
+    auto_deployments_enabled = true
+  }
+
+  instance_configuration {
+    cpu    = "1024"
+    memory = "2048"
   }
 }
 
-resource "aws_instance" "node_ec2_instance" {
-  ami             = data.aws_ami.ubuntu.id
-  instance_type   = "t2.micro"
-  security_groups = [aws_security_group.web_sg.name]
-
-  user_data = <<-EOF
-              #!/bin/bash
-              sudo yum update -y
-              sudo yum install docker -y
-              sudo service docker start
-              sudo docker run -d -p 80:3000 <ecr_image_url>
-              EOF
-
-  tags = {
-    Name = "chessio-backend"
-  }
+# Output the App Runner service URL
+output "app_runner_service_url" {
+  value = aws_apprunner_service.chessio_backend.service_url
 }
